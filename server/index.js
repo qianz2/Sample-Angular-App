32049da709288498c647c9c3b50cd1b702f22a2d
Cassidy Schaufele <cassidy@rsquaredtelecom.com>
2018-08-14 00:33:44 -0600

    Removed unneccessary directory in index.js static pathing

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use('/api', api);
 
 // Default index.html route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 /**
